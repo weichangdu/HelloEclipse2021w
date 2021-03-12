e0cff62388b40f31a53d7499dfd1fa28f971b45b
Weichang Du <wdu@unb.ca>
2021-03-12 16:02:52 -0400

    version 2

--- HelloProject/src/HellpApp.java
+++ HelloProject/src/HellpApp.java
@@ -3,7 +3,7 @@ public class HellpApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Weichang!!");
+		System.out.println("Hello Weichang Du!!");
 	}
 
 }
